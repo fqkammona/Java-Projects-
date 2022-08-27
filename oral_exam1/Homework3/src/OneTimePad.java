9b087a84c060b35559c170739f8c4dfc95163a25
Fatima Kammona <yourgitemail@example.com>
2022-08-27 17:05:15 -0500

    I finished the cipher for the easy part.

--- oral_exam1/Homework3/src/OneTimePad.java
+++ oral_exam1/Homework3/src/OneTimePad.java
@@ -22,6 +22,52 @@ public class OneTimePad {
         return message;
     }
 
+    public String CipherMessage(){
+        String OldMessage = message;
+        String newMessage = "";
+
+        int key = this.key;
+
+        int i = 0;
+        while(i < OldMessage.length()){ // While loop until the end of the message
+            int index = -1; // To know if it is a space
+            char letter = OldMessage.charAt(i); // Take the char of the old message, one letter at a time
+
+            for (int k = 0; k < alphabet.length; k++){ // For loop to find the index of the char
+                char tempLetter = alphabet[k];
+
+                if(letter == tempLetter){
+                    index = k;
+                }
+            }
+            // At this point we have the index //
+
+            String newLetter = "";
+
+            if(index == -1){ // If the index is still -1 then we know it is not a letter therefor it doesn't change
+                newLetter = String.valueOf(letter);
+            }else{
+                int newIndex = index + key;
+                if(newIndex > 25){ // if we are at the end of the alphabet
+                    newIndex = newIndex - 26; // subtract the alphabet. Since arrays start at 0 we need to subtract 26 instead of 25
+                }
+
+                // char newLetter = alphabet[newIndex]; // The new letter
+                newLetter = String.valueOf(alphabet[newIndex]); // converting the new letter from a char to a string
+            }
+
+            newMessage = newMessage + newLetter; // Adding the newLetter to the newMessage
+
+            i++;
+        }
+
+        return newMessage;
+    }
+
+    public static void printMessage(String message){ // This method prints out a string with spaces between each letter
+
+    }
+
     public static void main(String[] args) {
         OneTimePad myObj = new OneTimePad();
 
@@ -37,6 +83,8 @@ public class OneTimePad {
         myObj.setKey(key);
 
         System.out.println(myObj.getMessage());
+        System.out.println(myObj.CipherMessage());
+
 
 
 
