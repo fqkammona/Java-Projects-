2818d6696b8f6a9b09b84f989a882d331f7d0dbd
Fatima Kammona <yourgitemail@example.com>
2022-08-27 18:13:45 -0500

    I finished the encrypting for the middle part. I have been making seperate methods and not programs but I will just create those programs later and clean them up later.

--- oral_exam1/Homework3/src/OneTimePad.java
+++ oral_exam1/Homework3/src/OneTimePad.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
+import java.lang.Math; //importing this class so i can use math.Random
 
 public class OneTimePad {
     final static char alphabet [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     private int key;
+    private String keyList;
     private String message;
 
+    private void setKeyList(String keyList){
+        this.keyList = keyList;
+    }
+
+    private String getKeyList(){
+        return keyList;
+    }
     public void setKey(int key){
         this.key = key;
     }
@@ -107,6 +116,51 @@ public class OneTimePad {
         return decryptMess;
     }
 
+    public String EncryptMessageMedium(){
+        String oldMessage = message;
+        String encryptMess = "";
+        String keyString = "";
+        int newKey = -1;
+
+        int i = 0;
+        while(i < oldMessage.length()) { // While loop until the end of the message
+            int index = -1; // To know if the char is not a letter
+            char letter = oldMessage.charAt(i); // Take the char of the old message, one letter at a time
+
+            for (int k = 0; k < alphabet.length; k++) { // For loop to find the index of the char
+                char tempLetter = alphabet[k];
+
+                if (letter == tempLetter) {
+                    index = k;
+                }
+            }
+            // At this point we have the index //
+
+            String newLetter = "";
+
+            if(index == -1){ // If the index is still -1 then we know it is not a letter therefor it doesn't change
+                newLetter = String.valueOf(letter);
+                keyString = keyString + "  ";
+            }else{
+                newKey = (int)(Math.random() * 25);
+                int newIndex = index + newKey;
+                if(newIndex > 25){ // if we are at the end of the alphabet
+                    newIndex = newIndex - 26; // subtract the alphabet. Since arrays start at 0 we need to subtract 26 instead of 25
+                }
+
+                // char newLetter = alphabet[newIndex]; // The new letter
+                newLetter = String.valueOf(alphabet[newIndex]); // converting the new letter from a char to a string
+                keyString = keyString + Integer.toString(newKey) + ",";
+            }
+
+            encryptMess = encryptMess + newLetter; // Adding the newLetter to the newMessage
+
+            i++;
+        }
+
+        setKeyList(keyString);
+        return encryptMess;
+    }
     public static void printMessage(String message){ // This method prints out a string with spaces between each letter
 
     }
@@ -120,21 +174,24 @@ public class OneTimePad {
         String message = input.nextLine();
         myObj.setMessage(message);
 
-        System.out.println("Please enter your key!");
-        input = new Scanner(System.in);
-        int key = input.nextInt();
-        myObj.setKey(key);
+//        System.out.println("Please enter your key!");
+//        input = new Scanner(System.in);
+//        int key = input.nextInt();
+//        myObj.setKey(key);
+//
+//        System.out.println("Encrypting Message");
+//        System.out.println(myObj.getMessage());
+//        System.out.println(myObj.EncryptMessage());
+//
+//        myObj.setMessage(myObj.EncryptMessage());
+//
+//        System.out.println("Decrypting Message");
+//        System.out.println(myObj.getMessage());
+//        System.out.println(myObj.DecryptMessage());
 
-        System.out.println("Encrypting Message");
         System.out.println(myObj.getMessage());
-        System.out.println(myObj.EncryptMessage());
-
-        myObj.setMessage(myObj.EncryptMessage());
-
-        System.out.println("Decrypting Message");
-        System.out.println(myObj.getMessage());
-        System.out.println(myObj.DecryptMessage());
-
+        System.out.println(myObj.EncryptMessageMedium());
+        System.out.println(myObj.getKeyList());
 
 
 
