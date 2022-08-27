8c8c364dcbff5c5e7119626674384b1b01c5e8c2
Fatima Kammona <yourgitemail@example.com>
2022-08-27 17:26:21 -0500

    I finished the decrypting for the easy part.

--- oral_exam1/Homework3/src/OneTimePad.java
+++ oral_exam1/Homework3/src/OneTimePad.java
@@ -22,7 +22,7 @@ public class OneTimePad {
         return message;
     }
 
-    public String CipherMessage(){
+    public String EncryptMessage(){
         String OldMessage = message;
         String newMessage = "";
 
@@ -30,7 +30,7 @@ public class OneTimePad {
 
         int i = 0;
         while(i < OldMessage.length()){ // While loop until the end of the message
-            int index = -1; // To know if it is a space
+            int index = -1; // To know if the char is not a letter
             char letter = OldMessage.charAt(i); // Take the char of the old message, one letter at a time
 
             for (int k = 0; k < alphabet.length; k++){ // For loop to find the index of the char
@@ -64,6 +64,49 @@ public class OneTimePad {
         return newMessage;
     }
 
+    public String DecryptMessage(){
+        String encryptMess = message;
+        String decryptMess = "";
+
+        int key = this.key;
+
+        int i = 0;
+        while(i < encryptMess.length()){
+            int index = -1; // To know if the char is not a letter
+            char letter = encryptMess.charAt(i); // Take the char of the old message, one letter at a time
+
+            for (int k = 0; k < alphabet.length; k++){ // For loop to find the index of the char
+                char tempLetter = alphabet[k];
+
+                if(letter == tempLetter){
+                    index = k;
+                }
+            }
+
+            // At this point we have the index //
+
+            String newLetter = "";
+
+            if(index == -1){ // If the index is still -1 then we know it is not a letter therefor it doesn't change
+                newLetter = String.valueOf(letter);
+            }else{
+                int newIndex = index - key;
+                if(newIndex < 0){ // if we are at the begging of the alphabet
+                    newIndex = newIndex + 26; // subtract the alphabet. Since arrays start at 0 we need to subtract 26 instead of 25
+                }
+
+                // char newLetter = alphabet[newIndex]; // The new letter
+                newLetter = String.valueOf(alphabet[newIndex]); // converting the new letter from a char to a string
+            }
+
+            decryptMess = decryptMess + newLetter; // Adding the newLetter to the newMessage
+
+            i++;
+        }
+
+        return decryptMess;
+    }
+
     public static void printMessage(String message){ // This method prints out a string with spaces between each letter
 
     }
@@ -82,8 +125,16 @@ public class OneTimePad {
         int key = input.nextInt();
         myObj.setKey(key);
 
+        System.out.println("Encrypting Message");
         System.out.println(myObj.getMessage());
-        System.out.println(myObj.CipherMessage());
+        System.out.println(myObj.EncryptMessage());
+
+        myObj.setMessage(myObj.EncryptMessage());
+
+        System.out.println("Decrypting Message");
+        System.out.println(myObj.getMessage());
+        System.out.println(myObj.DecryptMessage());
+
 
 
 
