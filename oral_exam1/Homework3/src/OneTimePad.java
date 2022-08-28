1589067bcfd1d43a1abbe639e97f23058b313041
Fatima Kammona <yourgitemail@example.com>
2022-08-28 13:46:56 -0500

    Finished the meduim part. Its getting pretty messy and doesn't look nice. I am going to work on cleaning it up and breaking up the program.

--- oral_exam1/Homework3/src/OneTimePad.java
+++ oral_exam1/Homework3/src/OneTimePad.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math; //importing this class so i can use math.Random
+import java.lang.String;
 
 public class OneTimePad {
     final static char alphabet [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
@@ -140,7 +141,7 @@ public class OneTimePad {
 
             if(index == -1){ // If the index is still -1 then we know it is not a letter therefor it doesn't change
                 newLetter = String.valueOf(letter);
-                keyString = keyString + "  ";
+                keyString = keyString + " ,"; // Add a space and then a comma
             }else{
                 newKey = (int)(Math.random() * 25);
                 int newIndex = index + newKey;
@@ -161,8 +162,70 @@ public class OneTimePad {
         setKeyList(keyString);
         return encryptMess;
     }
-    public static void printMessage(String message){ // This method prints out a string with spaces between each letter
 
+    public String DecryptMessageMedium(){
+        String encryptMess = message;
+        String decryptMess = "";
+
+        String keylist = this.keyList;
+
+        int i = 0;
+        while(i < encryptMess.length()){
+            int index = -1; // To know if the char is not a letter
+            char letter = encryptMess.charAt(i); // Take the char of the old message, one letter at a time
+
+            for (int k = 0; k < alphabet.length; k++){ // For loop to find the index of the char
+                char tempLetter = alphabet[k];
+
+                if(letter == tempLetter){
+                    index = k;
+                }
+            }
+
+            // At this point we have the index //
+
+            String newLetter = "";
+
+            String[] hold = keylist.split("[,]",0); // Takes the string of numbers and splits it by commas and puts it in an array
+
+
+            if(index == -1){ // If the index is still -1 then we know it is not a letter therefor it doesn't change
+                newLetter = String.valueOf(letter);
+            }else{
+                int numberkey = Integer.parseInt(hold[i]); // changes the string to an integer
+                int newIndex = index - numberkey;
+                if(newIndex < 0){ // if we are at the begging of the alphabet
+                    newIndex = newIndex + 26; // subtract the alphabet. Since arrays start at 0 we need to subtract 26 instead of 25
+                }
+
+                // char newLetter = alphabet[newIndex]; // The new letter
+                newLetter = String.valueOf(alphabet[newIndex]); // converting the new letter from a char to a string
+            }
+
+            decryptMess = decryptMess + newLetter; // Adding the newLetter to the newMessage
+
+            i++;
+        }
+
+        return decryptMess;
+    }
+    public static void printMessage(String Nmessage){ // This method prints out a string with spaces between each letter
+
+        for (int i = 0; i < Nmessage.length(); i++) {
+           System.out.print(Nmessage.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public void printKey(){ // This method prints out a string with spaces between each letter
+
+        String[] newStr = keyList.split("[,]",0);
+
+        for (int i = 0; i < newStr.length; i++) {
+            System.out.print(newStr[i] + " ");
+        }
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -173,6 +236,26 @@ public class OneTimePad {
 
         String message = input.nextLine();
         myObj.setMessage(message);
+
+
+        System.out.print("Message:     ");
+        myObj.printMessage(message);
+
+        String newMessage = myObj.EncryptMessageMedium();
+
+        System.out.print("Key:         ");
+        myObj.printKey();
+
+        System.out.print("Cipher Text: ");
+        myObj.printMessage(newMessage);
+
+
+        myObj.setMessage(newMessage);
+        String deMessage = myObj.DecryptMessageMedium();
+
+        System.out.print("Decrypt Text: ");
+        myObj.printMessage(deMessage);
+
 
 //        System.out.println("Please enter your key!");
 //        input = new Scanner(System.in);
@@ -189,10 +272,25 @@ public class OneTimePad {
 //        System.out.println(myObj.getMessage());
 //        System.out.println(myObj.DecryptMessage());
 
-        System.out.println(myObj.getMessage());
-        System.out.println(myObj.EncryptMessageMedium());
-        System.out.println(myObj.getKeyList());
+//        System.out.println(myObj.getMessage());
+//
+//        myObj.setMessage(myObj.EncryptMessageMedium());
+//        String newMessage = myObj.getMessage();
+//
+//        System.out.println(newMessage);
+//        System.out.println(myObj.getKeyList());
+//
+//
+//        System.out.println(myObj.DecryptMessageMedium());
 
+//        String str = myObj.getKeyList();
+//        System.out.println("Message length = " + message.length());
+//        System.out.println("Length key = "+ str.length());
+//        String[] newStr = str.split("[,]",0);
+//        System.out.println("Length of newStr = " + newStr.length);
+//        for (int i = 0; i < newStr.length; i++) {
+//            System.out.print(newStr[i] + " ");
+//        }
 
 
 
