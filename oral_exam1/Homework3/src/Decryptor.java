edf0176e2c133bc639cc8f893422bd48c814416a
Fatima Kammona <yourgitemail@example.com>
2022-09-13 14:51:43 -0500

    Created the Decryptor Driver for by taking the main from decryptor and pasting it into the Decryptor driver.

--- oral_exam1/Homework3/src/Decryptor.java
+++ oral_exam1/Homework3/src/Decryptor.java
@@ -4,12 +4,12 @@ import java.io.FileReader; // Reads from file
 import java.io.BufferedReader;
 
 public class Decryptor {
-    final static char alphabet [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    final static char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     public String DecryptMessage(int position, String keyList, String message){
         position = position - 2; // You have to sub track by one because arrays start at zero
-        String[] keyArray = keyList.split("[,]",0); // makes an array(size 21) of strings of each element of
+        String[] keyArray = keyList.split(",",0); // makes an array(size 21) of strings of each element of
         String decryptedMessage = "";
 
         int i = message.length() - 1;
@@ -48,6 +48,8 @@ public class Decryptor {
 
             decryptedMessage =  newLetter + decryptedMessage; // Adding the newLetter to the newMessage and making sure it prints the correct way
 
+            // decryptedMessage.insert(0, newLetter);
+
             /* This section is the logic to loop the position */
             if(position == 0) { // If we get to the beginning of the array of keys
                 position = keyArray.length - 1; // Change the position to the start of the array
@@ -59,48 +61,9 @@ public class Decryptor {
         }
 
         return decryptedMessage;
+        // return decryptedMessage.toString();
     }
 
-    public static void main(String[] args) {
-        Decryptor decryptor = new Decryptor();
 
-        /* This section gets the path and opens the files */
-      //  System.out.println("This is the path: /Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/keyFile.txt");
-       // System.out.print("Please enter the path to the key file: ");
-
-       // Scanner keyPath = new Scanner(System.in);
-       // String key = keyPath.nextLine();
-        File keyFile = new File("/Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/keyFile.txt");
-
-      //  System.out.println("This is the path: /Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/keyFile.txtEncrypted.txt");
-       // System.out.print("Please enter the path to the encrypted file: ");
-
-       // Scanner encryptedPath = new Scanner(System.in);
-        //String encrypted = encryptedPath.nextLine();
-        File encryptedFile = new File("/Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/keyFile.txtEncrypted.txt");
-
-        try{
-            FileReader readKey = new FileReader(keyFile);
-            BufferedReader keyRead = new BufferedReader(readKey);
-
-            /* This section gets the position and keyList from the keyRead file */
-            String positionInput = keyRead.readLine();
-            int position = Integer.parseInt(positionInput); // Position needs to go 1 before than the give b/c arrays start at 0
-            String keyList = keyRead.readLine();
-
-            /* This section get the encrypted message */
-           Scanner encrypt = new Scanner(encryptedFile);
-           String encryptedMessage = encrypt.nextLine();
-
-            /* This section class the function and then prints out the message */
-
-           String decryptedMessage = decryptor.DecryptMessage(position,keyList, encryptedMessage);
-           System.out.println(decryptedMessage);
-           keyRead.close();
-        }
-        catch(Exception e) {
-            System.out.println("Incorrect path has been entered.");
-        }
-    }
 }
 
---
+++ oral_exam1/Homework3/src/DecryptorDriver.java
@@ -0,0 +1,48 @@
+import java.util.Scanner; // So that I write to the screen
+import java.io.File;
+import java.io.FileReader; // Reads from file
+import java.io.BufferedReader;
+
+public class DecryptorDriver {
+    public static void main(String[] args) {
+        Decryptor decryptor = new Decryptor();
+
+        /* This section gets the path and opens the files */
+        //  System.out.println("This is the path: /Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/keyFile.txt");
+        // System.out.print("Please enter the path to the key file: ");
+
+        // Scanner keyPath = new Scanner(System.in);
+        // String key = keyPath.nextLine();
+        File keyFile = new File("/Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/youWork.txt");
+
+        //  System.out.println("This is the path: /Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/keyFile.txtEncrypted.txt");
+        // System.out.print("Please enter the path to the encrypted file: ");
+
+        // Scanner encryptedPath = new Scanner(System.in);
+        //String encrypted = encryptedPath.nextLine();
+        File encryptedFile = new File("/Users/fatimakammona/Desktop/swd_fqkammona/oral_exam1/Homework3/src/Encrypted.youWork.txt");
+
+        try{
+            FileReader readKey = new FileReader(keyFile);
+            BufferedReader keyRead = new BufferedReader(readKey);
+
+            /* This section gets the position and keyList from the keyRead file */
+            String positionInput = keyRead.readLine();
+            int position = Integer.parseInt(positionInput); // Position needs to go 1 before than the give b/c arrays start at 0
+            String keyList = keyRead.readLine();
+
+            /* This section get the encrypted message */
+            Scanner encrypt = new Scanner(encryptedFile);
+            String encryptedMessage = encrypt.nextLine();
+
+            /* This section class the function and then prints out the message */
+
+            String decryptedMessage = decryptor.DecryptMessage(position,keyList, encryptedMessage);
+            System.out.println(decryptedMessage);
+            keyRead.close();
+        }
+        catch(Exception e) {
+            System.out.println("Incorrect path has been entered.");
+        }
+    }
+}
