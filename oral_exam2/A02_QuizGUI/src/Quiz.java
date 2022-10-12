88a8e39038cf708f7d0f42b5e46d667b86272326
Fatima Kammona <yourgitemail@example.com>
2022-10-11 23:24:09 -0500

    Added the radio button part and used group so that it toggles.

--- oral_exam2/A02_QuizGUI/src/Quiz.java
+++ oral_exam2/A02_QuizGUI/src/Quiz.java
@@ -4,20 +4,50 @@ import java.awt.event.ActionListener;
 
 public class Quiz implements ActionListener{
     public static JFrame quizFrame = new JFrame("Quiz");
-    public JCheckBox checkQuestion1, checkQuestion2, checkQuestion3;
-
+    public JCheckBox checkQuestion1, checkQuestion2, checkQuestion3,checkQuestion4;
+    public JRadioButton radioQuestion1, radioQuestion2, radioQuestion3, radioQuestion4;
     public Quiz() {
 
         /* Frame Settings */
-        quizFrame.setSize(700, 700);
+        quizFrame.setSize(500, 600);
 
         quizFrame.add(createCheckBox());
+        quizFrame.add(createRadiobutton());
         quizFrame.add(createButton());
 
         quizFrame.setVisible(true);
         quizFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ends the program when
     }
 
+    /** Creates the Radiobuttons and puts them in a group so that they toggle and
+     * adds them in a Jpanel and returns the panel.*/
+    private JPanel createRadiobutton(){
+        JPanel radioButtonPanel = new JPanel();
+
+        JLabel radioButtonTile = new JLabel("Which number is prime?");
+        radioButtonPanel.add(radioButtonTile);
+
+        radioButtonPanel.setLayout(new BoxLayout(radioButtonPanel, BoxLayout.PAGE_AXIS));
+        radioButtonPanel.setBounds(10, 170, 300, 200);
+
+        radioQuestion1 = new JRadioButton("21");
+        radioQuestion2 = new JRadioButton("1");
+        radioQuestion3 = new JRadioButton("73");
+        radioQuestion4 = new JRadioButton("39");
+
+        ButtonGroup group = new ButtonGroup(); // Allows the toggling
+        group.add(radioQuestion1);
+        group.add(radioQuestion2);
+        group.add(radioQuestion3);
+        group.add(radioQuestion4);
+
+        radioButtonPanel.add(radioQuestion1);
+        radioButtonPanel.add(radioQuestion2);
+        radioButtonPanel.add(radioQuestion3);
+        radioButtonPanel.add(radioQuestion4);
+
+        return radioButtonPanel;
+    }
     private JPanel createCheckBox() {
         /* Adding in checkbox section*/
         JPanel checkBoxPanel = new JPanel();
@@ -25,19 +55,17 @@ public class Quiz implements ActionListener{
         checkBoxPanel.add(checkBoxTitle);
 
         checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.PAGE_AXIS));
-        checkBoxPanel.setBounds(0, 10, 300, 200);
+        checkBoxPanel.setBounds(10, 10, 300, 150);
 
         checkQuestion1 = new JCheckBox("2 + 2 = 4");
         checkQuestion2 = new JCheckBox("3 *  3 = 9");
         checkQuestion3 = new JCheckBox("7 + 3 = 11");
-
-        checkQuestion1.setBounds(100, 100, 150, 50);
-        checkQuestion2.setBounds(100, 100, 150, 50);
-        checkQuestion3.setBounds(100, 100, 150, 50);
+        checkQuestion4 = new JCheckBox("33 / 5 = 6");
 
         checkBoxPanel.add(checkQuestion1);
         checkBoxPanel.add(checkQuestion2);
         checkBoxPanel.add(checkQuestion3);
+        checkBoxPanel.add(checkQuestion4);
 
         return checkBoxPanel;
     }
@@ -59,9 +87,13 @@ public class Quiz implements ActionListener{
 
     public void actionPerformed(ActionEvent e) {
         int numCorrect = 0;
-        if(checkQuestion1.isSelected()) { numCorrect++; }
+        if(checkQuestion1.isSelected()) {
+            numCorrect++;
+            checkQuestion1.setBorderPainted(true);
+        }
         if(checkQuestion2.isSelected()) { numCorrect++; }
 
+        if(radioQuestion3.isSelected()) { numCorrect++; }
         JOptionPane.showMessageDialog(quizFrame,
                 "Correct: " + numCorrect,
                 "Output",
