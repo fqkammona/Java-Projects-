3c2d0b1fed1274d4e7cc6612f719c988be712c40
Fatima Kammona <yourgitemail@example.com>
2022-10-15 21:08:21 -0500

    deleted the input box.

--- oral_exam2/A02_QuizGUI/src/Quiz.java
+++ oral_exam2/A02_QuizGUI/src/Quiz.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,25 +7,47 @@ public class Quiz implements ActionListener{
     public static JFrame quizFrame = new JFrame("Quiz");
     public JCheckBox checkQuestion1, checkQuestion2, checkQuestion3,checkQuestion4;
     public JRadioButton radioQuestion1, radioQuestion2, radioQuestion3, radioQuestion4;
+    public String[] dropdownOptions = {"1","3", "1.5","9", "6"};
     public Quiz() {
 
         /* Frame Settings */
-        quizFrame.setSize(500, 600);
+        quizFrame.setSize(500, 900);
 
         quizFrame.add(createCheckBox());
         quizFrame.add(createRadiobutton());
+        quizFrame.add(createDropdownBox());
         quizFrame.add(createButton());
 
         quizFrame.setVisible(true);
+        //quizFrame.setFont(new Font("Serif", Font.PLAIN, 26));
         quizFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ends the program when
     }
 
+    private JPanel createDropdownBox(){
+        JPanel dropdownBoxPanel = new JPanel();
+        dropdownBoxPanel.setBounds(10, 390, 300, 200);
+
+        JLabel dropdownQuestion = new JLabel("Select the answer to the question " +
+                "\n\n9-3*(1/3)+1");
+
+       // dropdownBoxPanel.setFont(new Font("Serif", Font.PLAIN, 14));
+        dropdownBoxPanel.add(dropdownQuestion);
+
+        JComboBox<String> dropDownBox = new JComboBox<>(dropdownOptions);
+        dropDownBox.setBounds(80, 50, 140, 20);
+
+        dropdownBoxPanel.add(dropDownBox);
+
+        return dropdownBoxPanel;
+    }
     /** Creates the Radiobuttons and puts them in a group so that they toggle and
      * adds them in a Jpanel and returns the panel.*/
     private JPanel createRadiobutton(){
         JPanel radioButtonPanel = new JPanel();
+       // radioButtonPanel.setFont(new Font("Serif", Font.PLAIN, 26));
 
         JLabel radioButtonTile = new JLabel("Which number is prime?");
+        radioButtonTile.setFont(new Font("Serif", Font.PLAIN, 22));
         radioButtonPanel.add(radioButtonTile);
 
         radioButtonPanel.setLayout(new BoxLayout(radioButtonPanel, BoxLayout.PAGE_AXIS));
@@ -41,11 +64,15 @@ public class Quiz implements ActionListener{
         group.add(radioQuestion3);
         group.add(radioQuestion4);
 
+        radioQuestion1.setFont(new Font("Serif", Font.PLAIN, 18));
+
         radioButtonPanel.add(radioQuestion1);
         radioButtonPanel.add(radioQuestion2);
         radioButtonPanel.add(radioQuestion3);
         radioButtonPanel.add(radioQuestion4);
 
+        radioButtonPanel.setEnabled(false);
+        //radioQuestion3.setEnabled(false);
         return radioButtonPanel;
     }
     private JPanel createCheckBox() {
@@ -74,10 +101,10 @@ public class Quiz implements ActionListener{
         /*Button section */
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(null);
-        buttonPanel.setBounds(60, 600, 200, 70);
+        buttonPanel.setBounds(60, 500, 200, 70);
 
         JButton submitButton = new JButton("Submit");
-        submitButton.setBounds(350, 550, 80, 30);
+        submitButton.setBounds(350, 450, 80, 30);
 
         submitButton.addActionListener(this);
         buttonPanel.add(submitButton);
@@ -87,16 +114,15 @@ public class Quiz implements ActionListener{
 
     public void actionPerformed(ActionEvent e) {
         int numCorrect = 0;
-        if(checkQuestion1.isSelected()) {
+        if (checkQuestion1.isSelected()) {
             numCorrect++;
             checkQuestion1.setBorderPainted(true);
         }
-        if(checkQuestion2.isSelected()) { numCorrect++; }
+        if (checkQuestion2.isSelected()) {
+            numCorrect++;
+        }
 
-        if(radioQuestion3.isSelected()) { numCorrect++; }
-        JOptionPane.showMessageDialog(quizFrame,
-                "Correct: " + numCorrect,
-                "Output",
-                JOptionPane.PLAIN_MESSAGE);
+        checkQuestion1.setForeground(Color.darkGray);
+
     }
 }
