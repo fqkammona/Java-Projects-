e5e1f0726d57187b86809326df19d9fc892bb8c4
Fatima Kammona <yourgitemail@example.com>
2022-10-27 09:30:38 -0500

    Added some notes to linkedlist.

--- oral_exam2/B06_LinkedListNet/src/LinkedList.java
+++ oral_exam2/B06_LinkedListNet/src/LinkedList.java
@@ -1,5 +1,25 @@
 // LinkedList by Fatima Kammona
 
-/** This is the enhanced linked list */
+/** This is the enhanced linked list.
+ * It allows for insert/delete anywhere in the linked list. */
 public class LinkedList<T> {
+    /** Instance Variables */
+    private Node<T> headNode;
+    private Node<T> tailNode;
+    private int size;
+
+    /** Accessor methods */
+    public int getSize() {return size;}
+    public boolean isEmpty(){ // returns true if the list is empty
+        return size==0;
+    }
+
+
+    // method insert anywhere in the list
+
+    // method delete anywhere in the list
+
+    // method fill linked list
+
+    // method print linked list
 }
