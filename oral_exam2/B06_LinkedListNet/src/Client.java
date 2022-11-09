3f35ea9b3f552ee39cdda748a0ecbff020e6a46d
fqkammona <fatima-kammona@uiowa.edu>
2022-11-09 09:25:58 -0600

    Working on the client interface. The buttons are in a container and the container is in a main JPanel that will is added to the Frame. I need to make all the buttons the same size.

--- oral_exam2/B06_LinkedListNet/src/Client.java
+++ oral_exam2/B06_LinkedListNet/src/Client.java
@@ -2,12 +2,9 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 /** This is the client class, it sends
@@ -19,49 +16,90 @@ public class Client extends JFrame {
     private JTextArea displayArea; // for displaying messages
     private DatagramSocket socket; // socket to connect to server
 
-    // set up GUI and DatagramSocket
-    public Client()
-    {
+    private JTextArea instructionArea; // for displaying instructions
+    private JTextArea replayArea; // for displaying replay
+
+    private JPanel displayPanel; // The panel that holds the TextAreas
+    private JPanel mainPanel = new JPanel(); // holds all the panels in it
+
+    private Container buttonContainer = new Container(); // Container that holds the buttons
+
+    private JButton printListButton = new JButton("Print List");
+    private JButton addButton = new JButton("Add Item");
+    private JButton deleteButton = new JButton("Delete Item");
+    private JButton endButton = new JButton("End Program");
+    private GridBagConstraints con = new GridBagConstraints();
+
+    private void fillContainerButtons() {
+        buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
+
+        printListButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttonContainer.add(printListButton);
+
+        addButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttonContainer.add(addButton);
+
+        deleteButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttonContainer.add(deleteButton);
+
+        endButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttonContainer.add(endButton);
+
+        con.gridx = 0; // first row
+        con.gridy = 0; // first col
+        con.fill = GridBagConstraints.HORIZONTAL;
+
+        mainPanel.add(buttonContainer, con);
+        add(mainPanel); // add to JFrame
+    }
+
+    private void fillJPanelDisplayPanel() {
+
+    }
+
+    //  set up GUI and DatagramSocket
+    public Client(String host) {
         super("Client");
+        mainPanel.setLayout(new GridBagLayout());
 
-        enterField = new JTextField("Type message here");
-        enterField.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent event)
-                    {
-                        try // create and send packet
-                        {
-                            // get message from textfield
-                            String message = event.getActionCommand();
-                            displayArea.append("\nSending packet containing: " +
-                                    message + "\n");
+        fillContainerButtons();
 
-                            byte[] data = message.getBytes(); // convert to bytes
+//
+//        enterField = new JTextField("Type message here");
+//        enterField.addActionListener(
+//                new ActionListener() {
+//                    public void actionPerformed(ActionEvent event) {
+//                        try // create and send packet
+//                        {
+//                            // get message from textfield
+//                            String message = event.getActionCommand();
+//                            displayArea.append("\nSending packet containing: " +
+//                                    message + "\n");
+//
+//                            byte[] data = message.getBytes(); // convert to bytes
+//
+//                            // create sendPacket
+//                            DatagramPacket sendPacket = new DatagramPacket(data,
+//                                    data.length, InetAddress.getByName(host), 23604);
+//
+//
+//                            socket.send(sendPacket); // send packet
+//                            displayArea.append("Packet sent\n");
+//                            displayArea.setCaretPosition(
+//                                    displayArea.getText().length());
+//                        } catch (IOException ioException) {
+//                            displayMessage(ioException + "\n");
+//                            ioException.printStackTrace();
+//                        }
+//                    }
+//                }
+//        );
+//
+//        mainPanel.add(enterField, BorderLayout.NORTH);
+//
+//        displayArea = new JTextArea();
+//        mainPanel.add(new JScrollPane(displayArea), BorderLayout.CENTER);
 
-                            // create sendPacket
-                            DatagramPacket sendPacket = new DatagramPacket(data,
-                                    data.length, InetAddress.getByName("128.255.17.116"), 23604);
-
-
-                            socket.send(sendPacket); // send packet
-                            displayArea.append("Packet sent\n");
-                            displayArea.setCaretPosition(
-                                    displayArea.getText().length());
-                        }
-                        catch (IOException ioException)
-                        {
-                            displayMessage(ioException + "\n");
-                            ioException.printStackTrace();
-                        }
-                    }
-                }
-        );
-
-        add(enterField, BorderLayout.NORTH);
-
-        displayArea = new JTextArea();
-        add(new JScrollPane(displayArea), BorderLayout.CENTER);
 
         setSize(400, 300); // set window size
         setVisible(true); // show window
@@ -69,13 +107,14 @@ public class Client extends JFrame {
         try // create DatagramSocket for sending and receiving packets
         {
             socket = new DatagramSocket();
-        }
-        catch (SocketException socketException)
-        {
+        } catch (SocketException socketException) {
             socketException.printStackTrace();
             System.exit(1);
         }
+
+        waitForPackets();
     }
+
 
     // wait for packets to arrive from Server, display packet contents
     public void waitForPackets()
--- oral_exam2/B06_LinkedListNet/src/TestClient.java
+++ oral_exam2/B06_LinkedListNet/src/TestClient.java
@@ -1,14 +1,24 @@
 // TestClient by Fatima Kammona
 
 import javax.swing.*;
+import javax.swing.JFrame;
+
 
 /** This class runs the client class application */
 
 public class TestClient {
     public static void main(String[] args)
     {
-        Client application = new Client(); // create client
+
+        Client application; // declare client application
+
+        // if no command line args
+        if (args.length == 0)
+            application = new Client("128.255.17.116"); // localhost
+        else
+            application = new Client(args[0]); // use args
+
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        application.waitForPackets(); // run client application
+
     }
 }
