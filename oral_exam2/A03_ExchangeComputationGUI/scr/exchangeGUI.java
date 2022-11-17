0278474c683a1720cd1f27f97c5ac2f32be5ce84
fqkammona <fatima-kammona@uiowa.edu>
2022-11-17 08:07:43 -0600

    Got the scene builder to show up when you run.

--- oral_exam2/A03_ExchangeComputationGUI/scr/exchangeGUI.java
+++ oral_exam2/A03_ExchangeComputationGUI/scr/exchangeGUI.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class exchangeGUI extends Application {
 
@@ -8,7 +13,18 @@ public class exchangeGUI extends Application {
     } // launch function calls the start function
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception {
+        URL myfxml = getClass().getResource("exchangeGUIfxml.fxml");
+        if(myfxml == null){
+            System.out.println("Bad ERROr");
+        }
+        else {
+            Parent root = FXMLLoader.load(myfxml);
 
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
