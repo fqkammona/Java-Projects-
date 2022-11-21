86ad115b9cda6721e7acfe0e315192a934a413f5
fqkammona <fatima-kammona@uiowa.edu>
2022-11-21 11:18:05 -0600

    I got the code and formate done for the login scene

--- oral_exam2/A03_ExchangeComputationGUI/scr/exchangeGUIController.java
+++ oral_exam2/A03_ExchangeComputationGUI/scr/exchangeGUIController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -21,16 +22,13 @@ import java.net.URL;
 import java.text.NumberFormat;
 
 public class exchangeGUIController {
-    // formatters for currency and percentages
     private Stage stage;
     private Scene scene;
     private Parent root;
-
     private boolean startBank = true; // to initizlle bank only once
-    private Bank bank;
+    public static Bank bank = new Bank();
 
     public void fillBank(){
-        bank = new Bank();
         bank.makeNewAccount(1,300);
         bank.makeNewAccount(2,1209.13);
         bank.makeNewAccount(3,10.0);
@@ -45,39 +43,55 @@ public class exchangeGUIController {
        stage.setScene(scene);
        stage.show();
 
-       if(startBank)
-           fillBank();
+     //  fillBank();
+//       if(startBank)
+//           fillBank();
    }
 
-   // When login button is pressed on the login page
-    @FXML
+   @FXML
+   private TextField accountNumber; // TextField for logIn page that holds account number
+
+    @FXML  // When login button is pressed on the login page
     private void loginAccountButtonPressed(ActionEvent event) throws IOException {
-        //  System.out.print("\nPlease enter your bank number: ");
-        //                        String foundAccount = bank.findAccount(input.nextInt());
-        //                        System.out.println(foundAccount);
-        //
-        //                        if(foundAccount.compareTo("Account has been found.") == 0){
-        //                            System.out.println("You are now logged in");
-        //                            logOn = true;
-        //                            output = bank.bankOptions();
-        //                        }
+        Alert alert = new Alert(Alert.AlertType.NONE); // Message box
 
+        /* This if statement checks
+        * 1. If textField accountNumber is empty or null, if so then an alert will be displayed.
+        * 2. If accountNumber has any letters in it, if so then an alert will be displayed.
+        * 3. If account has not been found then an alert will be displayed.
+        * 4. If account has been found then the account page will be displayed.*/
 
+        if (accountNumber.getText() == null || accountNumber.getText().trim().isEmpty()) { // if
+            alert.setAlertType(Alert.AlertType.ERROR); // set alert type
+            alert.setTitle("Please Enter Number: ");
+            alert.show(); // show the dialog
+        }
+        else if(!accountNumber.getText().matches("\\d*")){ // if any letters are inserted
+            alert.setAlertType(Alert.AlertType.ERROR); // set alert type
+            alert.setTitle("Please don't enter letters: ");
+            alert.show(); // show the dialog
+        }
+        else { // if a number has actually been entered
+            String foundAccount = bank.findAccount(Integer.parseInt(accountNumber.getText()));
+
+            if(foundAccount.compareTo("Account has been found.") == 0){
+                alert.setAlertType(Alert.AlertType.ERROR); // set alert type
+                alert.setTitle("You are now logged in");
+                alert.show(); // show the dialog
+            } else {
+                alert.setAlertType(Alert.AlertType.ERROR); // set alert type
+                alert.setTitle(foundAccount);
+                alert.show(); // show the dialog
+            }
+        }
     }
 
+    @FXML  // When login button is pressed on the login page
+    private void createAccount(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("exchangeGUIfxml.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
-
-/* ************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
