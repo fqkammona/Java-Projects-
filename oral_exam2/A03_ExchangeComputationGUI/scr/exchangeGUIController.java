5ad67db5b1ce94aaec88c834b901907e55e807bb
fqkammona <fatima-kammona@uiowa.edu>
2022-11-22 07:40:09 -0600

    Added a return home button to some of the pages.

--- oral_exam2/A03_ExchangeComputationGUI/scr/exchangeGUIController.java
+++ oral_exam2/A03_ExchangeComputationGUI/scr/exchangeGUIController.java
@@ -100,17 +100,17 @@ public class exchangeGUIController {
         stage.show();
     }
 
-    @FXML
-    private TextField balanceAmountDisplay;
+    //@FXML
+    //private TextField balanceAmountDisplay;
 
-    @FXML
-    private TextField numberAccountDisplay = new TextField(String.valueOf(bank.getAccountNum()));
+   // @FXML
+   // private TextField numberAccountDisplay = new TextField(String.valueOf(bank.getAccountNum()));
 
     /**
      * This method shows the mainAccountPagefxml scene.
      */
     private void takeMeToMainAccountPage(ActionEvent event) throws IOException {
-        balanceAmountDisplay.setText(bank.getBalance().toString());
+        //balanceAmountDisplay.setText(bank.getBalance().toString());
 
         root = FXMLLoader.load(getClass().getResource("mainAccountPagefxml.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
